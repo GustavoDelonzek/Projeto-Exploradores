c612d31ebf56d477b60395a14fbe236eefc79730
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-14 21:45:16 -0300

    Migrations criadas

---
+++ src/database/migrations/2025_03_14_210755_create_collectible_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collectible_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedDecimal('price', 8 ,2);
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collectible_items');
+    }
+};
